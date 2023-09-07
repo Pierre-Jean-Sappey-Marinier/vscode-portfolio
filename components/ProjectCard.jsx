a448678abaec5ae3b620933b5b7fa020e73619c9
Pierre-Jean-Sappey-Marinier <113429394+Pierre-Jean-Sappey-Marinier@users.noreply.github.com>
2023-09-07 19:14:40 +0200

    border fix

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -1,10 +1,24 @@
-import Image from 'next/image';
-import styles from '../styles/ProjectCard.module.css';
+import Image from "next/image";
+import styles from "../styles/ProjectCard.module.css";
 
 const ProjectCard = ({ project }) => {
   return (
     <div className={styles.card}>
-      <Image src={project.image} height={300} width={600} alt={project.name} />
+      <div className={styles.image}>
+        <Image
+          className="Image"
+          objectFit="contain"
+          src={project.image}
+          height={300}
+          width={600}
+          alt={project.name}
+        />
+        <style jsx global>{`
+          .Image {
+            border-radius: 20px;
+          }
+        `}</style>
+      </div>
       <div className={styles.content}>
         <h3>{project.name}</h3>
         <p>{project.description}</p>
--- pages/index.jsx
+++ pages/index.jsx
@@ -15,7 +15,7 @@ export default function HomePage() {
         <div className={styles.foreground}>
           <div className={styles.content}>
             <h1 className={styles.name}>Pierre-Jean SAPPEY-MARINIER</h1>
-            <h6 className={styles.bio}>Développeur web React Javascript</h6>
+            <h6 className={styles.bio}>Développeur Web React Javascript</h6>
             <Link href="/projects">
               <button className={styles.button}>Mes projets</button>
             </Link>
